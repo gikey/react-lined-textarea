d3f610df6c4522a2ddac7e1b34db53a9ae6e70b6
gikey <lupingxie@outlook.com>
2019-09-02 17:21:31 +0800

    refactor: update declaration file

--- types/react-lined-number.d.ts
+++ types/react-lined-number.d.ts
@@ -1,3 +1,16 @@
-import { IInPutProps } from "../src/index";
 
-declare function LinedTextarea(props: IInPutProps);
+import { ChangeEvent, ReactNode, TextareaHTMLAttributes } from "react";
+
+export type UnionOmit<T, K> = T & Omit<K, keyof T>;
+
+export interface ILinedTextarea {
+    lineHeight?: string | number;
+    value?: string;
+    renderLineNum?: (line: number) => ReactNode;
+    renderAddon?: (line: number) => ReactNode;
+    onChange?: (value: string, event: ChangeEvent<HTMLTextAreaElement>) => void;
+}
+
+export type IInPutProps = UnionOmit<ILinedTextarea, TextareaHTMLAttributes<HTMLTextAreaElement>>;
+
+declare function LinedTextarea(props: IInPutProps): JSX.Element;
